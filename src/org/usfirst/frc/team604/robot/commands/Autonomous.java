450469940374fb0481aeb01d6f40b717f9af599d
Jun Yang <yang.jun@outlook.com>
2015-01-12 22:02:56 -0800

    initial commit of skeleton code

---
+++ src/org/usfirst/frc/team604/robot/commands/Autonomous.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team604.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class Autonomous extends CommandGroup {
+
+}
---
+++ src/org/usfirst/frc/team604/robot/subsystems/Lock.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team604.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Lock extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void log() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
